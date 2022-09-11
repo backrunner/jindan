8685b99f7e7e04a7c03603bc69dbad7b7c05d6d2
BackRunner <dev@backrunner.top>
2022-09-12 01:41:49 +0800

    feat: add target parent property

--- src/types/manifest.ts
+++ src/types/manifest.ts
@@ -2,6 +2,7 @@ export interface JinDanManiFestItem {
   resourceType: 'script' | 'style';
   fileName: string;
   integrity?: string;
+  targetParent?: 'head' | 'body'; // Insert to body by default
 }
 
 export type JinDanManifest = JinDanManiFestItem[];
